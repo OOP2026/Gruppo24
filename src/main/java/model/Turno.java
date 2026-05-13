16abef6bfa8f12d5665d312306046be2f0a70891
Prozack245 <salvatore06.sg.sal06@gmail.com>
2026-05-13 19:23:21 +0200

    modifiche a turno

--- src/main/java/model/Turno.java
+++ src/main/java/model/Turno.java
@@ -5,26 +5,26 @@ import java.time.LocalTime;
 import utils.Validators; 
 
 public class Turno {
-    // Classe immutabile, creare un nuovo turno per apportare modifiche
-
+    
+    private final long idTurno; 
     private final DayOfWeek giornoDellaSettimana;
     private final LocalTime oraInizio;
     private final LocalTime oraFine;
-    private final StatoTurno statoTurno;
+   
 
-    public Turno(DayOfWeek giornoDellaSettimana, LocalTime oraInizio, LocalTime oraFine,StatoTurno statoTurno) {
+    public Turno(long idTurno, DayOfWeek giornoDellaSettimana, LocalTime oraInizio, LocalTime oraFine) {
 
         Validators.validaOggetto(giornoDellaSettimana, "Giorno della Settimana");
         Validators.validaIntervalloTempo(oraInizio, oraFine);
-
+        this.idTurno=idTurno; 
         this.giornoDellaSettimana = giornoDellaSettimana;
         this.oraInizio = oraInizio;
         this.oraFine = oraFine;
-        this.statoTurno=statoTurno; 
+         
     }
 
     public DayOfWeek getGiornoDellaSettimana() { return giornoDellaSettimana; }
     public LocalTime getOraInizio() { return oraInizio; }
     public LocalTime getOraFine() { return oraFine; }
-    public LocalTime getStatoTurno() { return statoTurno; }
+    public LocalTime getidTurno() { return idTurno; }
 }
