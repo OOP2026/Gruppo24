f565e5a353fae65167f8750e7c46a73fae12bc82
Prozack245 <salvatore06.sg.sal06@gmail.com>
2026-07-11 16:13:57 +0200

    solved exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignored) {}
         } catch (Exception e) {
             System.err.println("Impossibile impostare il Look and Feel di sistema: " + e.getMessage());
         }
