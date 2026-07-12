6ccafb95fb955a1cc3340ed5e3159187e7b63502
SydB99 <pi.musco@studenti.unina.it>
2026-07-12 15:42:26 +0200

    Created new util constants classes

--- src/main/java/utils/DateFormats.java
+++ src/main/java/utils/DateFormats.java
@@ -6,7 +6,7 @@ public final class DateFormats {
 
     public static final String DATE_FORMAT_PATTERN = "dd/MM/yyyy";
     public static final String DATE_TIME_FORMAT_PATTERN = "dd/MM/yyyy HH:mm";
-    public static final String TIME_FORMAT_PATTERN = "dd/MM/yyyy HH:mm";
+    public static final String TIME_FORMAT_PATTERN = "HH:mm";
 
     public static final DateTimeFormatter DATE_FMT  = DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN);
     public static final DateTimeFormatter DATETIME_FMT = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT_PATTERN);
---
+++ src/main/java/utils/Messages.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Messages {
+
+    public static final String DATE_FORMAT_MSG_USE = "Formato data non valido. Usare: ";
+    public static final String DATE_FORMAT_MSG = "Formato data non valido.";
+
+    public static final String ATTENZIONE_MSG = "Attenzione";
+
+    private Messages() {
+
+    }
+}
